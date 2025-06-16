b450716e6531e5d8aa7693da57d42467bfac008e
erickha16 <errickha@gmail.com>
2025-06-16 13:56:38 -0600

    Utilizar environment para el consumo de APIs

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -2,20 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from '../interfaces/Category.model';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  private readonly urlApi: string = 'https://localhost:7048/api/v1/categories';
+  // private readonly urlApi: string = 'https://localhost:7048/api/v1/categories';
+  // private readonly urlApi: string = environment.api.categories;
+
 
   //Inyección de dependencias
   constructor(private http: HttpClient) { }
 
   //Listado de categorias 
   public getCategories(): Observable<Category[]>{
-    return this.http.get<Category[]>(this.urlApi);
+    // return this.http.get<Category[]>(this.urlApi);
+    return this.http.get<Category[]>(environment.api.categories);
   }
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+    api: {
+        categories: 'https://localhost:7048/api/v1/categories',
+        brands: 'https://localhost:7048/api/v1/brands',
+        products: 'https://localhost:7048/api/v1/products',
+    }
+};
